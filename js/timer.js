45cfd405edb89061299ee264419876d72491dc57
interdigitize <interdigitize@users.noreply.github.com>
2016-10-25 15:22:52 -0700

    adjusted formatting and started working on adding a progress bar and a time input

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,11 @@
+//Used this tutorial as a base, https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
-    // 'total': t,
-    // 'days': days,
-    // 'hours': hours,
+    'total': t,
     'minutes': minutes,
     'seconds': seconds
   };
@@ -15,8 +13,6 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
@@ -27,17 +23,72 @@ function initializeClock(id, endtime) {
     // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
     }
+    // console.log("t is: " + t.total/1000);
+    // progress bar
+    // document.querySelector('#p1').addEventListener('mdl-componentupgraded', function() {
+    //   this.MaterialProgress.setProgress(t.total);
+    // });
   }
 
   updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+  var timeinterval = setInterval(updateClock, 1000); //add an input button to set the time field
 }
 
-var timeInMinutes = 1;
+var timeInMinutes = document.getElementById("sample2").value; //need to add an event listener for a value
+// console.log(timeInMinutes);
 var currentTime = Date.parse(new Date());
 var deadline = new Date(currentTime + timeInMinutes*60*1000);
 initializeClock('clockdiv', deadline);
+
+
+
+
+//
+// function getTimeRemaining(endtime) {
+//   var t = Date.parse(endtime) - Date.parse(new Date());
+//   var seconds = Math.floor((t / 1000) % 60);
+//   var minutes = Math.floor((t / 1000 / 60) % 60);
+//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//   return {
+//     // 'total': t,
+//     // 'days': days,
+//     // 'hours': hours,
+//     'minutes': minutes,
+//     'seconds': seconds
+//   };
+// }
+//
+// function initializeClock(id, endtime) {
+//   var clock = document.getElementById(id);
+//   var daysSpan = clock.querySelector('.days');
+//   var hoursSpan = clock.querySelector('.hours');
+//   var minutesSpan = clock.querySelector('.minutes');
+//   var secondsSpan = clock.querySelector('.seconds');
+//
+//   function updateClock() {
+//     var t = getTimeRemaining(endtime);
+//
+//     // daysSpan.innerHTML = t.days;
+//     // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+//
+//     if (t.total <= 0) {
+//       clearInterval(timeinterval);
+//     }
+//   }
+//
+//   updateClock();
+//   var timeinterval = setInterval(updateClock, 1000);
+// }
+//
+// var timeInMinutes = 1;
+// var currentTime = Date.parse(new Date());
+// var deadline = new Date(currentTime + timeInMinutes*60*1000);
+// initializeClock('clockdiv', deadline);
